f5458aa47f6bc6b8979bca0df7ebc551fc57368e
AKcs2310 <ankitkrgupta21@gmail.com>
2025-11-23 19:26:59 +0530

    initial commit

---
+++ modules/package/subpackages/__init__.py
@@ -0,0 +1,3 @@
+## __init__.py 
+## this file makes any thing as package 
+## that is subpackage is really a package because of the __init__.py
---
+++ modules/test.py
@@ -0,0 +1,6 @@
+from package.maths import *
+print(addition(2,4))
+print(subtraction(6,8))
+
+from package.subpackages.mult import *
+print(multiply(88,99))
